66e449187dee7ae330f03b6397a7c25565a59a10
Firas <firasdabbabi@gmail.com>
2023-03-04 20:40:57 +0100

    version 1.2: using {useRef} Hook in Level 4 component

--- src/components/Level4.jsx
+++ src/components/Level4.jsx
@@ -1,15 +1,27 @@
+import { useState, useRef } from "react";
+
 export default function Level4() {
-  const value = "here 4";
+  const [value, setValue] = useState("to be updated...");
+  const inputRef = useRef();
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+
+    setValue(inputRef.current.value);
+  };
+
   return (
     <div
       style={{
-        backgroundColor: "blue",
+        backgroundColor: "lightyellow",
         margin: "1rem",
         padding: "1.2rem",
       }}
     >
       <h2>Level 3</h2>
       <p>The value of level 4 is: {value}</p>
+      <input ref={inputRef} type="text" placeholder="Enter any value here" />
+      <button onClick={clickHandler}>Submit</button>
     </div>
   );
 }
